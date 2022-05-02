966139b2d727b57f1098eae0cf1d3b4fe007c22e
alissonandrade2020 <alisson2d@hotmail.com>
2022-05-02 16:10:36 -0300

    Iniciando o projeto

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,12 +1,24 @@
 import { ChatTeardropDots } from "phosphor-react";
+import { useState } from "react";
 
 export function Widget() {
+  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
+
+  function toggleWidgetVisibility() {
+    setIsWidgetOpen(!isWidgetOpen);
+  }
+
   return (
     <div className="absolute bottom-4 right-4">
-      <button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+      {isWidgetOpen && <p>Hello world</p>}
+
+      <button
+        onClick={toggleWidgetVisibility}
+        className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group"
+      >
         <ChatTeardropDots className="w-6 h-6" />
 
-        <span className="max-w-0 overflow-hidden group-hover:max-w-xs">
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
           <span className="pl-2"></span>
           Feedback
         </span>
