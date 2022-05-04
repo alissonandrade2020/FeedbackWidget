7372ef4c5b38661520c80a445cd7d8d9804e6507
alissonandrade2020 <alisson2d@hotmail.com>
2022-05-04 15:49:31 -0300

    Server

---
+++ server/anotacoes.md
@@ -0,0 +1,29 @@
+-  npm init -y
+
+- npm i typescript @types/node ts-node-dev -D
+
+- npx tsc --init
+
+- npm i express
+
+- npm i -D @types/express
+
+- npm i prisma -D  (DB)
+
+- npm i @prisma/client
+
+- npx prisma init
+
+- npx prisma migrate dev
+
+- npx prisma studio
+
+-  npm install nodemailer
+
+- npm i --save-dev @types/nodemailer
+
+(https://mailtrap.io/)
+
+- npm install jest -D
+
+- npx jest --init (y, y, node, y, v8, y)
---
+++ server/src/routes.ts
@@ -0,0 +1,58 @@
+import express from "express";
+import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
+import { PrismaFeedbacksRepository } from "./repositories/prisma/prisma-feedbacks-repository";
+import { NodemailerMailAdapter } from "./adapters/nodemailer/nodemailer-mail-adapter";
+
+export const routes = express.Router();
+
+// GET, POST, PUT, PATCH, DELETE, etc
+
+// GET = Buscar informações
+// POST = Cadastrar informações
+// PUT = Atualizar informaçoes de uma entidade
+// PATCH = Atualizar informação única de uma entidade
+
+routes.get("/", (req: any, res: any) => {
+  return res.send([
+    " ------------- < NLWReturn > --------------",
+    " |                                         |",
+    " |                Backend                  |",
+    " |                                         |",
+    " -------------- < Node.js > ---------------",
+    " |                                         |",
+    " |                  API                    |",
+    " |                                         |",
+    " ------------- < NLWReturn > --------------",
+    " |                                         |",
+    "         Alisson de Andrade Araújo         ",
+    "Formação: Análise e Desenvolvimento de Sistemas",
+    "Descrição: Desenvolvedor Back-end, Front-end e Mobile",
+    "Site: http://alissondeandradearaujo.000webhostapp.com/",
+    "CV: https://alissonandradesistema.000webhostapp.com/curriculo/",
+    "Currículo Lattes: http://lattes.cnpq.br/7594653859194302/",
+    "Github: https://github.com/alissonandrade2020",
+    "Rocketseat: https://app.rocketseat.com.br/me/alissondeandradearaujo",
+    "Linkedin: https://www.linkedin.com/in/alisson-de-andrade-ara%C3%BAjo-160224190/",
+  ]);
+});
+
+routes.post("/feedbacks", async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+  const nodemailerMailAdapter = new NodemailerMailAdapter();
+
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+    prismaFeedbacksRepository,
+    nodemailerMailAdapter
+  );
+
+  await submitFeedbackUseCase.execute({
+    type,
+    comment,
+    screenshot,
+  });
+  // console.log(req.body);
+
+  return res.status(201).send();
+});
