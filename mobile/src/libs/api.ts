b4c9d38f06275c2a887ac1e901eb67e3648bcc9d
alissonandrade2020 <alisson2d@hotmail.com>
2022-05-06 16:18:13 -0300

    Aula 5 - Deploy

--- mobile/src/libs/api.ts
+++ mobile/src/libs/api.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const api = axios.create({
   // baseURL: 'http://localhost:3333',
-  baseURL: 'http://192.168.1.4:3333',
-
+  // baseURL: 'http://192.168.1.4:3333',
+  baseURL: 'https://feedbackwidget-production.up.railway.app/',
 });
